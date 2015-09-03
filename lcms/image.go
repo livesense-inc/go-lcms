b25dbaad7879a18af5dd705c6c4b6122e271bd79
Yoshihiro Yamazaki <yoya@awm.jp>
2015-09-03 17:58:42 +0900

    RGBA64 決め打ちではあるが、それ以外のColorModelが来た場合にエラーを返す処理 - 引数の RGBA64 を image.Image インターフェースに変更 - RGBA64 以外を受け取ったら err オブジェクトを返

--- lcms/image.go
+++ lcms/image.go
@@ -1,18 +1,28 @@
 package lcms
 
 import (
+	"fmt"
 	"image"
+	"image/color"
 )
 
-func ImageTransformByProfile(src_image *image.RGBA, src_prof Profile, dst_prof Profile) (*image.RGBA, error) {
-	rect := src_image.Bounds()
-	dst_image := image.NewRGBA(rect)
-	//
+func ImageTransformByProfile(src_image image.Image, src_prof Profile, dst_prof Profile) (image.Image, error) {
+	var dst_image image.Image
 	transform := CreateTransform(&src_prof, TYPE_RGBA_8, &dst_prof, TYPE_RGBA_8)
-	//
-	src_pix := src_image.Pix
-	dst_pix := dst_image.Pix
-	len_pix := len(src_pix)
-	transform.DoTransform(src_pix, dst_pix, len_pix)
+	rect := src_image.Bounds()
+
+	colorModel := src_image.ColorModel()
+	switch colorModel {
+	case color.RGBAModel:
+		src_rgba64 := src_image.(*image.RGBA64) // type assertions
+		dst_rgba64 := image.NewRGBA(rect)
+		src_pix := src_rgba64.Pix
+		dst_pix := dst_rgba64.Pix
+		len_pix := len(src_pix)
+		transform.DoTransform(src_pix, dst_pix, len_pix)
+		dst_image = image.Image(dst_rgba64)
+	default:
+		return nil, fmt.Errorf("ImageTransformByProfile: Unsupported ColorModel(%d)", colorModel)
+	}
 	return dst_image, nil
 }
