1a2d0b816199861ab8b3d38af12af4083c3e3a8e
Yoshihiro Yamazaki <yoya@awm.jp>
2015-09-03 18:01:12 +0900

    CreateTransform が RGBA64 用なので、switch case の中に入れた。

--- lcms/image.go
+++ lcms/image.go
@@ -8,12 +8,12 @@ import (
 
 func ImageTransformByProfile(src_image image.Image, src_prof Profile, dst_prof Profile) (image.Image, error) {
 	var dst_image image.Image
-	transform := CreateTransform(&src_prof, TYPE_RGBA_8, &dst_prof, TYPE_RGBA_8)
 	rect := src_image.Bounds()
 
 	colorModel := src_image.ColorModel()
 	switch colorModel {
 	case color.RGBAModel:
+		transform := CreateTransform(&src_prof, TYPE_RGBA_8, &dst_prof, TYPE_RGBA_8)
 		src_rgba64 := src_image.(*image.RGBA64) // type assertions
 		dst_rgba64 := image.NewRGBA(rect)
 		src_pix := src_rgba64.Pix
